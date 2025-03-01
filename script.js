428e2698a72ef4807650f43d3c389f1e2957b3a3
HiyaRahmaan <rahmaanhiya@gmail.com>
2025-03-02 04:36:07 +0600

    second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.getElementById('QnA').addEventListener("click", 
+    function(){
+        window.location.href="QnA.html"
+    })
+
+
+document.getElementById('BackToHome').addEventListener("click", 
+     function BackToHome(){
+            console.log(3)
+    })
+
+    
+document.getElementById('Completed').addEventListener("click",
+    function(){
+        console.log(7)
+    })
+
+    document.getElementById('clearHistory').addEventListener("click", 
+        function(){
+            alert()
+
+        })
+    
